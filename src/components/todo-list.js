ed8bf9abf5bd82cb9217c99203d7c1f54604c0de
rbarakhvostov <rbarakhvostov@gmail.com>
2019-09-30 11:44:56 +0300

    add structure

---
+++ src/components/todo-list.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import TodoListItem from './todo-list-item';
+
+const TodoList = ({ todos }) => {
+  const elements = todos.map((item) => {
+    console.log(item)
+    return (
+      <li>
+        <TodoListItem
+          label={item.label}
+          important={item.important} />
+      </li>
+    )
+  })
+  return (
+    <ul>
+      { elements }
+    </ul>
+  )
+}
+
+export default TodoList;
