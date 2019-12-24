990e301bb8e07e2b356a467ab5ca229f05725eaf
rbarakhvostov <rbarakhvostov@gmail.com>
2019-12-24 23:40:59 +0300

    change class component to functional component

--- src/components/item-status-filter/item-status-filter.js
+++ src/components/item-status-filter/item-status-filter.js
@@ -1,32 +1,33 @@
-import React, { Component } from 'react';
+import React from 'react';
 
 import './item-status-filter.css';
 
-export default class ItemStatusFilter extends Component {
-  buttons = [
+const ItemStatusFilter = ({ onFilter, filter }) => {
+  const buttonsInfo = [
     {name: 'all', label: 'ALL'}, 
     {name: 'active', label: 'ACTIVE'},
     {name: 'done', label: 'DONE'}
   ];
-  render() {
-    const { onFilter, filter } = this.props;
-    const buttons = this.buttons.map(({ name, label }) => {
-      const className = name === filter
-                ? 'filter active btn btn-outline-primary btn-lg'
-                : 'filter btn btn-outline-primary btn-lg';
-      return (
-        <button className={ className }
-                type='button'
-                key={ name }
-                onClick={ () => onFilter(name) }>
-          { label }
-        </button>
-      )
-    });
+
+  const buttons = buttonsInfo.map(({ name, label }) => {
+    const className = name === filter
+              ? 'filter active btn btn-outline-primary btn-lg'
+              : 'filter btn btn-outline-primary btn-lg';
     return (
-      <div className='filter-wrap btn-group'>
-        { buttons }
-      </div>
-    );
-  }
+      <button className={ className }
+              type='button'
+              key={ name }
+              onClick={ () => onFilter(name) }>
+        { label }
+      </button>
+    )
+  });
+
+  return (
+    <div className='filter-wrap btn-group'>
+      { buttons }
+    </div>
+  );
 }
+
+export default ItemStatusFilter;
